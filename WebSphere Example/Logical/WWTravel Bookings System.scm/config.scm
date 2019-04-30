d090d727561bb00bb11505d311ee20f887a622b8
c_hag <c_hag@presales1>
2019-04-30 21:03:11 +0100

    Initial API Test Commit

---
+++ WebSphere Example/Logical/WWTravel Bookings System.scm/config.scm
@@ -0,0 +1,3 @@
+<?xml version="1.0" encoding="UTF-8"?><editableResource createdTimestamp="1328024477900" createdUser="Chris" id="1b5ae254:1353467bacf:-7edb" type="service_component_resource" updatedTimestamp="1328024477900" updatedUser="Chris" version="1.1">
+   <loggingConfiguration debug="false" writeStubEventsToDB="false"/>
+</editableResource>
